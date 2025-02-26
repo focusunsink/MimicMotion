86dc78a65a1b186aed4de4c7d88448cdd7713de3
focusunsink <549303468@qq.com>
2025-02-26 12:47:16 +0800

    Update env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -4,3 +4,8 @@ apt-get install libglib2.0-0
 apt-get install libgl1-mesa-glx
 ldconfig -p | grep libGLX
 ldconfig
+
+# cudnn
+find /usr -name libcudnn_ops.so.9 
+cat /usr/include/cudnn_version.h | grep CUDNN_MAJOR -A 2  # Check the cuDNN version
+export LD_LIBRARY_PATH=/usr/local/lib/python3.10/dist-packages/nvidia/cudnn/lib:$LD_LIBRARY_PATH 
