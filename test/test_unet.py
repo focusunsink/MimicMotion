3092299a092f7412140f6f556300bfe346ba4993
focusunsink <549303468@qq.com>
2025-03-03 13:23:50 +0800

    add test

--- test/test_unet.py
+++ test/test_unet.py
@@ -37,25 +37,38 @@ unet.load_state_dict(unet_ckpt)
 unet.eval()
 unet = unet.to("cuda:0").half()
 
+data = torch.load("unet_input_output.pt")
 
-latent_model_input = torch.zeros([1, 16, 8, 128, 72]).to("cuda:0").half()
-t = torch.tensor(1.6377).to("cuda:0").half().reshape([1])
-image_embeddings = torch.zeros([1, 1, 1024]).to("cuda:0").half()
-added_time_ids = torch.zeros([1, 3]).to("cuda:0").half()
 
-# unet = torch.compile(unet)
+latent_model_input = data["latent_model_input"]
+t = data["t"]
+encoder_hidden_states = data["encoder_hidden_states"]
+added_time_ids = data["added_time_ids"]
+pose_latents = data["pose_latents"]
+_noise_pred_gt = data["_noise_pred"]
+
+print("latent_model_input", latent_model_input.shape, latent_model_input.dtype)
+print("t", t.shape, t.dtype)
+print("encoder_hidden_states", encoder_hidden_states.shape, encoder_hidden_states.dtype)
+print("added_time_ids", added_time_ids.shape, added_time_ids.dtype)
+print("pose_latents", pose_latents.shape, pose_latents.dtype)
+print("_noise_pred_gt", _noise_pred_gt.shape, _noise_pred_gt.dtype)
+
+
+#unet = torch.compile(unet)
 import time
 
 
 with torch.no_grad():
-    for i in range(10):
+    for i in range(5):
         torch.cuda.synchronize()
         st = time.time()
         _noise_pred = unet(
                             latent_model_input,
                             t,
-                            encoder_hidden_states=image_embeddings,
+                            encoder_hidden_states=encoder_hidden_states,
                             added_time_ids=added_time_ids,
+                            pose_latents = pose_latents,
                         )[0]
 
         torch.cuda.synchronize()
@@ -63,6 +76,11 @@ with torch.no_grad():
         print("unet time used: ", ed - st)
 
 
-print(_noise_pred)
+print("_noise_pred", _noise_pred)
 print(_noise_pred.dtype)
-print(_noise_pred.shape)
+
+print("simimlarity ", torch.cosine_similarity(_noise_pred_gt, _noise_pred))
+err = torch.abs(_noise_pred - _noise_pred_gt)
+sum_err = torch.sum(err)
+print("err ", err)
+print("sum err", sum_err)
